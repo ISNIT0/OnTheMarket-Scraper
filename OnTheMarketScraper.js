95aa5dd9df3ba178dd8fad6bfb33f3c0692cf06c
Joe <joseph.reeve@googlemail.com>
2015-04-19 12:46:46 +0100

    Added closes to modals

---
+++ OnTheMarketScraper.js
@@ -0,0 +1,19 @@
+var request = require('sync-request');
+var cheerio = require('cheerio');
+
+module.exports = {
+  byOutcode:function(outcode){
+    return JSON.parse(request('GET', "https://mobile-api.onthemarket.com/api/v1/property/search?location-id="+outcode+"&new-home=false&page=0&search-type=for-sale&under-offer=false", {
+      headers:{
+        'Api-Token':'923f89e2-cddc-4c56-999d-2fd1e4a8e5de'
+      }
+    }).getBody());
+  },
+  detail:function(id){
+    return JSON.parse(request('GET', "https://mobile-api.onthemarket.com:443/api/v1/property/"+id, {
+      headers:{
+        'Api-Token':'923f89e2-cddc-4c56-999d-2fd1e4a8e5de'
+      }
+    }).getBody());
+  }
+};
