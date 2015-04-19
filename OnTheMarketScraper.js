227b95c92ada04c6ca7e77503c88273cc229e8d2
Joe <joseph.reeve@googlemail.com>
2015-04-19 12:55:21 +0100

    Fixed Some Issues

--- OnTheMarketScraper.js
+++ OnTheMarketScraper.js
@@ -2,8 +2,8 @@ var request = require('sync-request');
 var cheerio = require('cheerio');
 
 module.exports = {
-  byOutcode:function(outcode){
-    return JSON.parse(request('GET', "https://mobile-api.onthemarket.com/api/v1/property/search?location-id="+outcode+"&new-home=false&page=0&search-type=for-sale&under-offer=false", {
+  byOutcode:function(outcode, page){
+    return JSON.parse(request('GET', "https://mobile-api.onthemarket.com/api/v1/property/search?location-id="+outcode+"&new-home=false&page="+(page||0)+"&search-type=for-sale&under-offer=false", {
       headers:{
         'Api-Token':'923f89e2-cddc-4c56-999d-2fd1e4a8e5de'
       }
