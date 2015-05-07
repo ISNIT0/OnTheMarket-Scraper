95c301fd50996de4aed47713bce67600baa60171
Joe Reeve <joe@wigwamm.com>
2015-05-07 10:53:52 +0100

    updated token

--- OnTheMarketScraper.js
+++ OnTheMarketScraper.js
@@ -5,14 +5,14 @@ module.exports = {
   byOutcode:function(outcode, page){
     return JSON.parse(request('GET', "https://mobile-api.onthemarket.com/api/v1/property/search?location-id="+outcode+"&new-home=false&page="+(page||0)+"&search-type=for-sale&under-offer=false", {
       headers:{
-        'Api-Token':'923f89e2-cddc-4c56-999d-2fd1e4a8e5de'
+        'Api-Token':'8d70de5f-53e9-487d-a511-346a145cf99b'
       }
     }).getBody());
   },
   detail:function(id){
     return JSON.parse(request('GET', "https://mobile-api.onthemarket.com:443/api/v1/property/"+id, {
       headers:{
-        'Api-Token':'923f89e2-cddc-4c56-999d-2fd1e4a8e5de'
+        'Api-Token':'8d70de5f-53e9-487d-a511-346a145cf99b'
       }
     }).getBody());
   }
